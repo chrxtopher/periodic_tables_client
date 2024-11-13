0c84063ea6847f5e98e202f1449ecca154aae987
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-13 16:41:27 -0700

    api file that holds functions used to access api

---
+++ src/utility/api.js
@@ -0,0 +1,139 @@
+const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3500";
+
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+/**
+ * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
+ *
+ * This function is NOT exported because it is not needed outside of this file.
+ *
+ * @param url
+ *  the url for the requst.
+ * @param options
+ *  any options for fetch
+ * @param onCancel
+ *  value to return if fetch call is aborted. Default value is undefined.
+ * @returns {Promise<Error|any>}
+ *  a promise that resolves to the `json` data or an error.
+ *  If the response is not in the 200 - 399 range the promise is rejected.
+ */
+const fetchJson = async (url, options, onCancel) => {
+  try {
+    const response = await fetch(url, options);
+    const payload = await response.json();
+
+    if (response.status === 204) return null;
+
+    if (payload.error) {
+      return Promise.reject({ message: payload.error });
+    }
+    return payload.data;
+  } catch (error) {
+    if (error.name !== "AbortError") {
+      console.error(error.stack);
+      throw error;
+    }
+    return Promise.resolve(onCancel);
+  }
+};
+
+export async function listReservations(params, signal) {
+  const url = new URL(`${API_BASE_URL}/reservations`);
+  Object.entries(params).forEach(([key, value]) =>
+    url.searchParams.append(key, value.toString())
+  );
+  return await fetchJson(url, { headers, signal }, [])
+    .then(formatReservationDate)
+    .then(formatReservationTime);
+}
+
+export async function createReservation(reservation, signal) {
+  const url = `${API_BASE_URL}/reservations`;
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ data: reservation }),
+    signal,
+  };
+  return await fetchJson(url, options, reservation);
+}
+
+export async function readReservation(reservation_id, signal) {
+  const url = `${API_BASE_URL}/reservations/${reservation_id}`;
+  const options = {
+    method: "GET",
+    headers,
+    signal,
+  };
+
+  return await fetchJson(url, options).then(formatReservationDate);
+}
+
+export async function createTable(table, signal) {
+  const url = `${API_BASE_URL}/tables`;
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ data: table }),
+    signal,
+  };
+  return await fetchJson(url, options, table);
+}
+
+export async function listTables(signal) {
+  const url = `${API_BASE_URL}/tables`;
+  return await fetchJson(url, { signal });
+}
+
+export async function seatReservation(reservation_id, table_id, signal) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: "PUT",
+    headers,
+    body: JSON.stringify({ data: { reservation_id } }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
+
+export async function updateReservation(reservation, signal) {
+  const url = `${API_BASE_URL}/reservations/${reservation.reservation_id}`;
+  const options = {
+    method: "PUT",
+    headers,
+    body: JSON.stringify({ data: reservation }),
+    signal,
+  };
+  return await fetchJson(url, options, reservation);
+}
+
+export async function cancelReservation(reservation_id, signal) {
+  const url = `${API_BASE_URL}/reservations/${reservation_id}/status`;
+  const options = {
+    method: "PUT",
+    body: JSON.stringify({ data: { status: "cancelled" } }),
+    headers,
+    signal,
+  };
+  return await fetchJson(url, options);
+}
+
+export async function finishTable(table_id) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: "DELETE",
+  };
+  return await fetchJson(url, options);
+}
+
+export async function updateReservationStatus(reservation, signal) {
+  const url = `${API_BASE_URL}/reservations/${reservation.reservation_id}/status`;
+  const options = {
+    method: "PUT",
+    headers,
+    body: JSON.stringify({ reservation }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
