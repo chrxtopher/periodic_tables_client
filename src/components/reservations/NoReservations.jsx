47d95bece0ae630002b071e1329ddf8c70fb669b
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-13 17:08:30 -0700

    Created a card to display if no reservations are booked

---
+++ src/components/reservations/NoReservations.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoReservations = () => {
+  return (
+    <div className="card border-light bg-light m-4 shadow-lg">
+      <div className="card-body">
+        <h4 className="card-text text-center">No Reservations</h4>
+      </div>
+    </div>
+  );
+};
+
+export default NoReservations;
