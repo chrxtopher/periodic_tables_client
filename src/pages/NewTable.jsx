d9d16afedd2508d85f240c4408e76046f029d2f8
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-17 22:06:43 -0700

    NewTable page that will hold the table form

---
+++ src/pages/NewTable.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TableForm from "../components/tables/TableForm";
+
+const NewTable = () => {
+  return (
+    <>
+      <h1 className="display-4 text-center mt-3">New Table</h1>
+      <TableForm />
+    </>
+  );
+};
+
+export default NewTable;
